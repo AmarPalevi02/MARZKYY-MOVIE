c68f38c5836647231c77b7b0cfc35719c2111bde
amarNrddn <amarnrddn02@gmail.com>
2024-01-03 13:32:02 +0700

    update card

--- src/app/components/Card/index.js
+++ src/app/components/Card/index.js
@@ -1,12 +1,17 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
-const Card = ({ title, images }) => {
+const Card = ({ title, images, id }) => {
     return (
-        <div>
-            <h1>{title}</h1>
-            <Image src={images} alt='...' width={600} height={600}/>
-        </div>
+        <Link href={`/${id}`} >
+            <div className='relative'>
+                <div className="w-full h-[200px] flex">
+                    <Image src={images} alt='notfound' width={250} height={200} className='object-cover' />
+                </div>
+                <h1 className='text-white mt-2 line-clamp-2 pl-2 pb-3'>{title}</h1>
+            </div>
+        </Link >
     )
 }
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,16 @@
 import Card from "./components/Card"
 
 export default async function Home() {
-  const getData = await fetch(`${process.env.NEXT_PUBLIC__API_BASE_URL}/top/anime`)
+  const getData = await fetch(`${process.env.NEXT_PUBLIC__API_BASE_URL}/top/anime?limit=8`)
     .then(response => response.json())
   const anime = getData.data
 
   return (
-    <div>
-      {anime.map((item, i) => {
+    <div className='grid md:grid-cols-4 sm:grid-cols-3 gap-2'>
+      {anime.map((item) => {
         return (
-          <div key={i}>
-            <Card title={item.title} images={item.images.webp.image_url} />
+          <div key={item.mal_id} className="">
+            <Card title={item.title} images={item.images.webp.image_url} id={item.mal_id}/>
           </div>
         )
       })}
