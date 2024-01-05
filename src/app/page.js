343d2af4746b30bb23ee6d9f3f522bbda559b392
amarNrddn <amarnrddn02@gmail.com>
2024-01-06 02:35:34 +0700

    create popular

--- src/app/page.js
+++ src/app/page.js
@@ -7,12 +7,12 @@ export default async function Home() {
   const anime = getData.data
 
   return (
-    <div>
-      <Description>Popular</Description>
+    <div className="pt-5">
+      <Description>#Popular</Description>
       <div className='grid md:grid-cols-4 sm:grid-cols-3 gap-2'>
         {anime.map((item) => {
           return (
-            <div key={item.mal_id} className="">
+            <div key={item.mal_id} className="bg-slate-900">
               <Card title={item.title} images={item.images.webp.image_url} id={item.mal_id} />
             </div>
           )
