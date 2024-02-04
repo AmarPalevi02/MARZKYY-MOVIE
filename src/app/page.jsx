eca5f6fa8d36f666a02f604c6864cfdfaa4ddbf7
amarNrddn <amarnrddn02@gmail.com>
2024-02-04 23:18:37 +0700

    update last

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,34 +4,28 @@ import Card from "../components/Card"
 import Description from "../components/Description"
 import { useEffect, useState } from "react"
 import { getTopAnime, getRekomendedAnime, reproduce } from "@/utils/fetch"
-import { authSeason } from "@/utils/auth"
-import {authOption} from "@/app/api/auth/[...nextauth]/route"
 
 export default function Home() {
   const [topAnime, setTopAnime] = useState([])
   const [rekomended, setRekomended] = useState([])
   const [isLoading, setIsLoading] = useState(false)
-  const [authUser, setAuthUser] = useState(null)
 
   const getTop = async () => {
     setIsLoading(true)
     const getData = await getTopAnime("top/anime", "limit=8")
     let rekomendedAnime = await getRekomendedAnime("recommendations/anime", "entry")
-    let authuser = await authSeason(authOption)
     setTopAnime(getData.data)
     setRekomended(rekomendedAnime = reproduce(rekomendedAnime, 8))
-    setAuthUser(authuser)
-    console.log(authuser)
     setIsLoading(false)
   }
 
   useEffect(() => {
     getTop()
-  }, [authUser])
+  }, [])
 
   return (
     <>
-      <Navbar auth={authUser} />
+      <Navbar />
       <section className="md:pt-20 sm:pt-20 xs:pt-0">
         <Description>#Popular</Description>
         <Card api={topAnime} isLoading={isLoading} />
--- src/components/Navbar/UserLogin.jsx
+++ src/components/Navbar/UserLogin.jsx
@@ -1,28 +1,22 @@
 "use client"
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { authSeason } from "@/utils/auth"
-import { authOption } from "@/app/api/auth/[...nextauth]/route"
 import { FaRegUser } from "react-icons/fa";
 import Link from 'next/link';
 
-const UserLogin = ({ auth }) => {
+const UserLogin = () => {
     const user = async () => {
         const user = await authSeason()
-        console.log(user)
+        return user
     }
-
-    const authUser = user().name
-
-
-    console.log(authUser)
-    // console.log(auth)
+    const authUser = user()
 
     return (
         <>
-            <Link href={auth ? '/api/auth/signout' : '/api/auth/signin'}
+            <Link href={authUser ? '/api/auth/signout' : '/api/auth/signin'}
                 className="w-[30px] h-[30px] bg-[#272829] rounded-full flex justify-center items-center text-center"
             >
-                {auth ? "LoutOut" : "Login"}
+                {authUser ? <FaRegUser /> : <FaRegUser />}
             </Link>
         </>
 
